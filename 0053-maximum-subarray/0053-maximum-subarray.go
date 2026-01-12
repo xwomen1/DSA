5d5a922dccfa7ee08bbe2b081b211efb4c9d9b36
xwomen1 <103394848+xwomen1@users.noreply.github.com>
2026-01-12 14:51:00 +0700

    Time: 3 ms (16.17%), Space: 10.6 MB (35.17%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.go
+++ 0053-maximum-subarray/0053-maximum-subarray.go
@@ -1,53 +1,16 @@
 func maxSubArray(nums []int) int {
-    return divideAndConquer(nums, 0, len(nums)-1)
-}
-
-func divideAndConquer(nums []int, left, right int) int {
-  
-    if left == right {
-        return nums[left]
+  maxSum := nums[0]
+  currentSum := nums[0]
+  for i :=1; i<len(nums); i++{
+    if currentSum <0 {
+        currentSum = nums[i]
+    }else {
+        currentSum += nums[i]
     }
 
-    mid := (left + right) / 2
-
-    // 1. Tìm Max bên trái (đệ quy)
-    leftMax := divideAndConquer(nums, left, mid)
-    
-   
-    rightMax := divideAndConquer(nums, mid+1, right)
-    
-   
-    crossMax := maxCrossingSum(nums, left, mid, right)
-
-   
-    return max(leftMax, max(rightMax, crossMax))
-}
-
-func maxCrossingSum(nums []int, left, mid, right int) int {
-   
-    sum := 0
-    leftPart := -100000 
-    for i := mid; i >= left; i-- {
-        sum += nums[i]
-        if sum > leftPart {
-            leftPart = sum
-        }
+    if currentSum > maxSum{
+        maxSum = currentSum
     }
-
-    
-    sum = 0
-    rightPart := -100000
-    for i := mid + 1; i <= right; i++ {
-        sum += nums[i]
-        if sum > rightPart {
-            rightPart = sum
-        }
-    }
-
-    return leftPart + rightPart
-}
-
-func max(a, b int) int {
-    if a > b { return a }
-    return b
+  }
+  return maxSum
 }
