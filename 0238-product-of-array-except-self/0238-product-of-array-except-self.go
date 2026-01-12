73eb8837f2dc2dd46a2aaf250c9d641b11ef1cb5
xwomen1 <103394848+xwomen1@users.noreply.github.com>
2026-01-12 10:07:19 +0700

    Time: 0 ms (100%), Space: 10.8 MB (25.39%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.go
@@ -0,0 +1,17 @@
+func productExceptSelf(nums []int) []int {
+    n := len(nums)
+    result := make([]int, n)
+    
+    result[0] = 1
+    for i := 1; i < n; i++ {
+        result[i] = result[i-1] * nums[i-1]
+    }
+
+    right := 1
+    for i := n - 1; i >= 0; i-- {
+        result[i] = result[i] * right
+        right = right * nums[i]
+    }
+
+    return result
+}
