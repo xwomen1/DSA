db169354c5869916efb19d685f8edd780b3ee74b
xwomen1 <103394848+xwomen1@users.noreply.github.com>
2026-01-13 15:52:37 +0700

    Time: 22 ms (60.28%), Space: 10 MB (28.99%) - LeetHub

--- 0015-3sum/0015-3sum.go
+++ 0015-3sum/0015-3sum.go
@@ -2,41 +2,31 @@ func threeSum(nums []int) [][]int {
     n := len(nums)
     sort.Ints(nums)
     result := [][]int{}
+    for i:=0; i<n-2; i++{
+        if nums[i]>0 {break}
+        if i>0 && nums[i]==nums[i-1]{continue}
+        right, left := n-1, i+1
+        for left < right {
+            sum := nums[i] + nums[left] + nums[right]
+            if sum==0{
+                result = append(result, []int{nums[i], nums[left], nums[right]})
 
-    for i :=0; i< n-2; i++{
-        if nums[i]>0{
-            break
-        }
+                for left < right && nums[left]==nums[left+1]{
+                    left++
+                }
 
-         if i>0 && nums[i]==nums[i-1]{
-        continue
-    }
-
-    left, right := i+1, n-1
-    for left <right {
-        sum:= nums[i] + nums[left]+ nums[right]
-
-        if sum ==0{
-            result = append(result, []int{nums[i],nums[left], nums[right]})
-
-            for left < right && nums[left] ==nums[left+1]{
+                for left<right && nums[right]==nums[right-1]{
+                    right--
+                }
                 left++
-            }
-            for left < right && nums[right] ==nums[right-1] {
+                right--
+            }else if sum <0 {
+                left++
+            }else{
                 right--
             }
-            left++
-            right--
-        }else if sum <0{
-            left++
-        }else{
-            right--
         }
-    }
-    }
 
-   
-    
+    }
     return result
-
 }
