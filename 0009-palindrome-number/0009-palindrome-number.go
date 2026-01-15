10fd0567db1eeffb2e5f125ae28f70dedc9e36bf
xwomen1 <103394848+xwomen1@users.noreply.github.com>
2026-01-15 10:57:02 +0700

    Time: 0 ms (100%), Space: 6 MB (76.08%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.go
+++ 0009-palindrome-number/0009-palindrome-number.go
@@ -2,13 +2,12 @@ func isPalindrome(x int) bool {
 	if x <0 || (x%10 ==0 &&  x!=0) {
         return false
     }
-    orgin := x
     reversed := 0
-   for x > 0 {
+   for x > reversed {
        y := x%10
        reversed = y + reversed*10
        x=x/10
    }
-   return reversed == orgin
+   return reversed == x || reversed/10 == x
     
 }
