3d279f8f05829c686f6de10f93ae3bb2ec286e54
xwomen1 <103394848+xwomen1@users.noreply.github.com>
2026-01-15 11:27:56 +0700

    Time: 2 ms (49.43%), Space: 4.7 MB (57.27%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.go
+++ 0013-roman-to-integer/0013-roman-to-integer.go
@@ -1,22 +1,23 @@
 func romanToInt(s string) int {
-  var values [128]int
-    values['I'] = 1
-    values['V'] = 5
-    values['X'] = 10
-    values['L'] = 50
-    values['C'] = 100
-    values['D'] = 500
-    values['M'] = 1000
+  m := map[byte]int{
+    'I' : 1,
+    'V' : 5,
+    'X': 10,
+    'L': 50,
+    'C': 100,
+    'D': 500,
+    'M': 1000,
+  }
 
-    sum := 0
-    n := len(s)
-    for i := 0; i < n; i++ {
-        val := values[s[i]]
-        if i+1 < n && val < values[s[i+1]] {
-            sum -= val
-        } else {
-            sum += val
-        }
-    }
-    return sum
+  value := 0
+  n := len(s)
+  for i:=0 ; i< n; i++{
+    currentVal := m[s[i]]
+    if i < n -1 && currentVal < m[s[i+1]] {
+        value -= currentVal
+    }else {
+        value += currentVal
+    }  
+  }
+  return value
 }
